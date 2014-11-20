38b5c26a12dea5e26f79220e34d6f88de8bd8089
Todd Hayton <thayton@neekanee.com>
2014-11-20 10:01:28 -0500

    put code into class

--- ntu-edu.py
+++ ntu-edu.py
@@ -14,28 +14,16 @@
 # USAGE: 
 #   $ ./ntu-edu.py
 ######################################################################
-import sys, logging
+import sys, signal, logging
 import mechanize, time
 from bs4 import BeautifulSoup, Comment
 
 URL = 'https://wish.wis.ntu.edu.sg/webexe/owa/aus_subj_cont.main'
 DELAY = 5
 
-def soupify(page):
-    s = BeautifulSoup(page)
-
-    # Remove unwanted tags
-    tags = s.findAll(lambda tag: tag.name == 'script' or \
-                                 tag.name == 'style')
-    for t in tags:
-        t.extract()
-        
-    # Remove comments
-    comments = s.findAll(text=lambda text:isinstance(text, Comment))
-    for c in comments:
-        c.extract()
-
-    return s
+def sigint(signal, frame):
+    sys.stderr.write('Exiting...\n')
+    sys.exit(0)    
 
 def select_form(form):
     '''
@@ -43,63 +31,104 @@ def select_form(form):
     '''
     return form.attrs.get('target', None) == 'subjects'
 
-def submit_form(item):
-    '''
-    Submit form using selection item.name and write the results
-    to file named according to item.label
-    '''
-    maxtries = 3
-    numtries = 0
+class NtuEduScraper:
+    def __init__(self):
+        self.br = mechanize.Browser()
+        self.url = 'https://wish.wis.ntu.edu.sg/webexe/owa/aus_subj_cont.main'
+        self.delay = 5
+        self.items = []
 
-    sys.stderr.write('Submitting form for item %s\n' % item.name)
+    def soupify(self, page):
+        '''
+        Run HTML page through BeautifulSoup and return the results, 
+        minus script/style tags and comments
+        '''
+        s = BeautifulSoup(page)
 
-    br = mechanize.Browser()
+        # Remove unwanted tags
+        tags = s.findAll(lambda tag: tag.name == 'script' or \
+                                     tag.name == 'style')
 
-    while numtries < maxtries:
-        try:
-            br.open(URL)
-            br.select_form(predicate=select_form)
-            br.form['r_course_yr'] = [ item.name ]
-            br.form.find_control('boption').readonly = False
-            br.form['boption'] = 'CLoad'
-            br.submit()
-            break
-        except (mechanize.HTTPError, mechanize.URLError) as e:
-            if isinstance(e,mechanize.HTTPError):
-                print e.code
-            else:
-                print e.reason.args
+        for t in tags:
+            t.extract()
+        
+        # Remove comments
+        comments = s.findAll(text=lambda text:isinstance(text, Comment))
+        for c in comments:
+            c.extract()
+
+        return s
+
+    def submit_form(self, item):
+        '''
+        Submit form using selection item.name and write the results
+        to file named according to item.label
+        '''
+        maxtries = 3
+        numtries = 0
+
+        sys.stderr.write('Submitting form for item %s\n' % item.name)
+
+        while numtries < maxtries:
+            try:
+                self.br.open(self.url)
+                self.br.select_form(predicate=select_form)
+                self.br.form['r_course_yr'] = [ item.name ]
+                self.br.form.find_control('boption').readonly = False
+                self.br.form['boption'] = 'CLoad'
+                self.br.submit()
+                break
+            except (mechanize.HTTPError, mechanize.URLError) as e:
+                if isinstance(e,mechanize.HTTPError):
+                    print e.code
+                else:
+                    print e.reason.args
 
             numtries += 1
-            time.sleep(numtries * DELAY)
+            time.sleep(numtries * self.delay)
 
-    if numtries == maxtries:
-        raise
+        if numtries == maxtries:
+            raise
 
-    data = br.response().read()
+        self.item_results_to_file(item, self.br.response().read())
 
-    label = ' '.join([label.text for label in item.get_labels()])
-    label = '-'.join(label.split())
+    def item_results_to_file(self, item, results):
+        label = ' '.join([label.text for label in item.get_labels()])
+        label = '-'.join(label.split())
 
-    sys.stderr.write('Writing results for item %s to file %s.html\n' % (item.name, label))
+        sys.stderr.write('Writing results for item %s to file %s.html\n' % (item.name, label))
 
-    with open("%s.html" % label, 'w') as f:
-        f.write(data)
-        f.close()
-    
+        with open("%s.html" % label, 'w') as f:
+            f.write(results)
+            f.close()
+        
+    def get_items(self):
+        '''
+        Get the list of items in the second dropdown of the form
+        '''
+        sys.stderr.write('Generating list of items for form selection\n')
+
+        self.br.open(self.url)
+        self.br.select_form(predicate=select_form)
+
+        items = self.br.form.find_control('r_course_yr').get_items()
+
+        sys.stderr.write('Got %d items for form selection\n' % len(items))
+
+        return items
+
+    def scrape(self):
+        self.items = self.get_items()
+
+        for item in self.items:
+            # Skip invalid/blank item selections
+            if len(item.name) < 1:
+                continue
+
+            self.submit_form(item)
+            time.sleep(3)
+        
 if __name__ == '__main__':
-    br = mechanize.Browser()
-    br.open(URL)
-    br.select_form(predicate=select_form)
-
-    sys.stderr.write('Generating list of items for form selection\n')
-
-    items = br.form.find_control('r_course_yr').get_items()
-
-    for item in items:
-        # Skip invalid/blank item selections
-        if len(item.name) < 1:
-            continue
-
-        submit_form(item)
-        time.sleep(3)
+    signal.signal(signal.SIGINT, sigint)
+    scraper = NtuEduScraper()
+    scraper.scrape()
